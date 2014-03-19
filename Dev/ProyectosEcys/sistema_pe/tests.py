db2660b0a7f65315c782b0742306248cad2be4bd
ojr <orfrant@gmail.com>
2014-03-19 00:41:43 -0600

    separacion de pruebas en diferentes clases V4

--- Dev/ProyectosEcys/sistema_pe/tests.py
+++ Dev/ProyectosEcys/sistema_pe/tests.py
@@ -7,9 +7,9 @@ from sistema_pe import login
 #Create your tests here.
 
 
-class Unittest_Login(TestCase):
+#class Unittest_Login(TestCase):
 
-    def preparar(self):
+'''    def preparar(self):
         c = sha512('clave').hexdigest()
         c2 = sha512('clave2').hexdigest()
         Usuario.objects.create(carnet='200819222', clave=c)
@@ -29,6 +29,24 @@ class Unittest_Login(TestCase):
         usFalso = login.obtener_usuario('nombreFalso')
         usVerdadero = login.obtener_usuario('200819222')
         self.assertIsNone(usFalso)
-        self.assertIsNotNone(usVerdadero)
+        self.assertIsNotNone(usVerdadero)  '''
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 
+class Selenium_test(LiveServerTestCase):
+
+    def preparar(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicity_wait(3)
+
+    def bajar(self):
+        self.browser.quit()
+
+
+    def pueden_entrar_a_admin_site(self):
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django admin', body.text)
+        self.fail('fin de prueba')
